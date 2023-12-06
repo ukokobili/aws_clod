db98f3ed91d588a03848329da3bc04eb444bb926
ukokobili <ukokobili.jacob@gmail.com>
2023-12-06 20:43:47 +0100

    added s3_object_function dictionary output

--- lambda_files/s3_object_function.py
+++ lambda_files/s3_object_function.py
@@ -7,3 +7,11 @@ def lambda_handler(event, context):
     crash_key = event["crash_key"]
     vehicles_key = event["vehicles_key"]
     people_key = event["people_key"] 
+
+    crash_response = s3.get_object(Bucket=bucket_name, Key=crash_key) 
+    vehicles_response = s3.get_object(Bucket=bucket_name, Key=vehicles_key) 
+    people_response = s3.get_object(Bucket=bucket_name, Key=people_key) 
+    
+    # Step 4: Extract data from each response and store it as a list of dictionaries for further processing:
+    return {"crash_response": crash_response, "vehicles_response":vehicles_response, 
+            "people_response": people_response}
